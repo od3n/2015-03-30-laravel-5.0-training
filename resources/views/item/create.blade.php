772a0da6f72992b2fe77ab61a6e7cd089afe4a05
Salahuddin Hairai <mr.od3n@gmail.com>
2015-03-30 17:03:50 +0800

    Edit Item

--- resources/views/item/create.blade.php
+++ resources/views/item/create.blade.php
@@ -5,16 +5,19 @@
 </head>
 <body>
 
+@if (isset($item->id))
 	{!! Form::open(['action' => 'ItemController@store']) !!}
-
+@else
+	{!! Form::open(['action' => 'ItemController@update', $item->id]) !!}
+@endif
 	<label>Description</label>
-	{!! Form::text('name', null) !!}
+	{!! Form::text('name', ($item->id) ? $item->name : Input::get('name')) !!}
 	<br />
 	<label>Description</label>
-	{!! Form::textarea('description', null) !!}
+	{!! Form::textarea('description', ($item->id) ? $item->description : Input::get('description')) !!}
 	<br />
 	<label>Active</label>
-	{!! Form::checkbox('active', 1, true) !!}
+	{!! Form::checkbox('active', 1, ($item->id) ? ($item->active) ? true : false : Input::get('description')) !!}
 
 	<br />
 	{!! Form::submit() !!}
