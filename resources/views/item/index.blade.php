469127301dedf472f805598c048bfc75a6abeb57
Salahuddin Hairai <mr.od3n@gmail.com>
2015-03-31 10:23:30 +0800

    Use link_to_route for links

--- resources/views/item/index.blade.php
+++ resources/views/item/index.blade.php
@@ -2,5 +2,6 @@ List
 
 <ul>
 @foreach ($items as $item)
+	<li>{!! link_to_route('item.show', $item->name, [$item->id]) !!} {!! link_to_route('item.edit', 'Edit', [$item->id]) !!}</li>
 @endforeach
 </ul>
